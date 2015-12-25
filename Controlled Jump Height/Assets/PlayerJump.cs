317599bade916496436159e6142297696a9294d3
Gkxd <gkxd@users.noreply.github.com>
2015-12-25 11:35:31 -0500

    Added Controlled Jump Height

---
+++ Controlled Jump Height/Assets/PlayerJump.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerJump : MonoBehaviour {
+
+    public float gravity;
+    public float jumpForce;
+    public float jumpSpeed;
+
+    private new Rigidbody rigidbody;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidbody.AddForce(Vector3.up * jumpForce);
+        }
+
+        rigidbody.AddForce(Vector3.down * gravity);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rigidbody.velocity += Vector3.up * jumpSpeed;
+        }
+    }
+}
