5e411be628154192aa40cad4b2e414f66f3b67d2
Gkxd <gkxd@users.noreply.github.com>
2016-01-02 23:55:50 -0500

    Added Ramp Texture Animation

---
+++ Ramp Texture Animation/Assets/SlideRampTexture.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Renderer))]
+public class SlideRampTexture : MonoBehaviour {
+
+    public float speed;
+
+    private Material m;
+    void Start() {
+        m = GetComponent<Renderer>().material;
+    }
+
+    void Update() {
+        Vector2 offset = new Vector2(-Time.time * speed % 1, 0);
+        m.SetTextureOffset("_MainTex", offset);
+    }
+}
